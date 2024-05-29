27201fba19c3b50801b7df91864bbceeb118cc0e
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-29 22:43:52 +0600

    get api for admin role

--- index.js
+++ index.js
@@ -86,6 +86,14 @@ async function run() {
             res.send(result);
         })
 
+        // Get all the user information from DB
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
